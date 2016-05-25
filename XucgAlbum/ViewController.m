bb82502363eac49532d3c2c79590594b9c59d0a0
许晨光 <xuchenguang@letv.com>
2016-05-25 11:03:21 +0800

    create project

---
+++ XucgAlbum/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XucgAlbum
+//
+//  Created by xucg on 5/25/16.
+//  Copyright © 2016 xucg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
